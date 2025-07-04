4fc9663c7c0e471d3eb2ff9753ba40f6c311dd57
Cyrille <jb4.cmorales2024@mngtvi.edu.ph>
2025-07-04 09:42:02 +0800

    remove render fragment

--- TechnicalAsssesment/Layout/MainLayout.razor.cs
+++ TechnicalAsssesment/Layout/MainLayout.razor.cs
@@ -6,7 +6,6 @@ namespace TechnicalAsssesment.Layout
     public partial class MainLayoutBase : LayoutComponentBase
     {
         [Inject] protected AppStateService _appState { get; set; } = default!;
-        [Parameter] public RenderFragment UserComponent { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
